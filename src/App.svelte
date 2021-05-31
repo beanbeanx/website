42c3d108eab8f31cba6af6658eae480328c33a0d
Nick Hynes <nhynes@oasislabs.com>
2021-05-31 11:14:05 -0500

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import Tailwind from './Tailwind.svelte';
+
+  export let name: string;
+</script>
+
+<main class="text-center p-4 max-w-xs mx-auto sm:max-w-none">
+  <h1 class="text-red-500 text-6xl uppercase leading-loose font-thin">BeanBean Exchange</h1>
+</main>
