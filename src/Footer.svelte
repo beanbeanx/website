8fb4f7877c451795477e052938ca2388eec83055
Shay Dong <shayd@berkeley.edu>
2021-06-05 19:55:37 -0500

    added contact and Blog links to header and footer

--- src/Footer.svelte
+++ src/Footer.svelte
@@ -7,30 +7,17 @@
     <div class="w-full flex flex-col md:flex-row py-6">
       <Logo/>
 
-      <div class="flex-1">
-        <p class="uppercase text-gray-500 md:mb-6">Social</p>
-        <ul class="list-reset mb-6">
-          <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-            <a href="#" class="no-underline hover:underline text-gray-800 hover:text-pink-500">Facebook</a>
-          </li>
-        </ul>
-      </div>
-      <div class="flex-1">
-        <p class="uppercase text-gray-500 md:mb-6">Social</p>
-        <ul class="list-reset mb-6">
-          <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-            <a href="#" class="no-underline hover:underline text-gray-800 hover:text-pink-500">Facebook</a>
-          </li>
-        </ul>
-      </div>
-      <div class="flex-1">
-        <p class="uppercase text-gray-500 md:mb-6">Company</p>
-        <ul class="list-reset mb-6">
-          <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-            <a href="#" class="no-underline hover:underline text-gray-800 hover:text-pink-500">Official Blog</a>
-          </li>
-        </ul>
-      </div>
+        <div class="w-full flex-grow lg:flex lg:items-center lg:w-auto hidden mt-2 lg:mt-0 bg-white lg:bg-transparent text-black p-4 lg:p-0 z-20" id="nav-content">
+          <ul class="list-reset lg:flex justify-end flex-1 items-center">
+  
+            <li class="mr-3">
+              <a class="inline-block text-gray-500 no-underline py-2 px-4" href="https://beanbean.substack.com">Blog</a>
+            </li>
+            <li class="mr-3">
+              <a class="inline-block text-gray-500 no-underline py-2 px-4" href="mailto: fcontact@beanbean.exchange">Contact</a>
+            </li>
+          </ul>
+        </div>
 
     </div>
   </div>
