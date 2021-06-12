6c2e3059838d67400fe54e85de47cc0b9be17b92
Nick Hynes nick@beanbean.exchange <nhynes@oasislabs.com>
2021-06-12 17:10:13 -0500

    Use SvelteKit for better SEO

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import "../app.postcss";
+  import Nav from "$lib/Nav.svelte";
+  import Footer from "$lib/Footer.svelte";
+</script>
+
+<Nav />
+
+<main>
+  <slot />
+</main>
+
+<Footer />
