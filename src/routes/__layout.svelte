e6c0f3879e80abbdd8c2e370807e749124491e61
Nick Hynes <nick@beanbean.exchange>
2021-06-21 09:32:49 -0500

    Add title section

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -11,3 +11,31 @@
 </main>
 
 <!-- <Footer /> -->
+<footer>
+  <section id="signup">
+    <h1>Stay up with the latest news &amp; Get $5 off at launch</h1>
+    <iframe
+      title="mailing list signup via substack"
+      src="https://beanbean.substack.com/embed"
+      width="360"
+      height="150"
+      frameborder="0"
+      scrolling="no"
+    />
+  </section>
+</footer>
+
+<style lang="scss">
+  @use "src/breakpoints" as bkpt;
+
+  main {
+    background-color: #f6f3f0;
+    @media (max-width: bkpt.$mobile) {
+      margin-top: 9vh;
+    }
+  }
+
+  footer {
+    background-color: white;
+  }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,4 +3,44 @@
   import { scrollTo } from '../utils.ts';
 </script>
 
-<PointerCards />
+<svelte:head>
+  <title>BeanBean Exchange - Blue Mountain Coffee, Verified Using Blockchain</title>
+  <meta
+    name="description"
+    content="BeanBean Exchange makes it fun and easy to buy authentic Blue Mountain Coffee directly from farms. Built on blockchain for full verifiability and tokenized rewards."
+  />
+</svelte:head>
+
+<div>
+  <section id="welcome">
+    <hgroup>
+      <h1>Buy the world's best coffee directly from farms.</h1>
+      <h2 class="welcome-subheading">
+        Enjoy authentic Jamaican Blue Mountain Coffee
+        delivered directly from farms with end-to-end verifiability,
+        provided by blockchain.
+      </h2>
+    </hgroup>
+
+    <button>Subscribe to Updates</button>
+  </section>
+
+  <section id="how-it-works">
+    <h2>How it works</h2>
+    <PointerCards />
+  </section>
+</div>
+
+<style lang="scss">
+  section {
+    padding: 0.5px 0;
+  }
+
+  .welcome-message {
+
+  }
+
+  .welcome-subheading {
+    font-weight: normal;
+  }
+</style>
